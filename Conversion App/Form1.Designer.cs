02d4d14b81d1cbbb07a5da007e676fe0a2d2e88e
archergs <archergardinersheridan@gmail.com>
2018-05-07 09:01:57 +0930

    Initial Release
    Updater doesnt work yet

---
+++ Conversion App/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Conversion_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dropDown = new System.Windows.Forms.ComboBox();
+            this.dropDownTwo = new System.Windows.Forms.ComboBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userValue = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dropDown
+            // 
+            this.dropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dropDown.FormattingEnabled = true;
+            this.dropDown.Items.AddRange(new object[] {
+            "mm",
+            "cm",
+            "m",
+            "km"});
+            this.dropDown.Location = new System.Drawing.Point(41, 176);
+            this.dropDown.Name = "dropDown";
+            this.dropDown.Size = new System.Drawing.Size(151, 26);
+            this.dropDown.TabIndex = 0;
+            this.dropDown.SelectedIndexChanged += new System.EventHandler(this.dropDown_SelectedIndexChanged);
+            // 
+            // dropDownTwo
+            // 
+            this.dropDownTwo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropDownTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dropDownTwo.FormattingEnabled = true;
+            this.dropDownTwo.Items.AddRange(new object[] {
+            "mm",
+            "cm",
+            "m",
+            "km"});
+            this.dropDownTwo.Location = new System.Drawing.Point(337, 176);
+            this.dropDownTwo.Name = "dropDownTwo";
+            this.dropDownTwo.Size = new System.Drawing.Size(151, 26);
+            this.dropDownTwo.TabIndex = 1;
+            this.dropDownTwo.SelectedIndexChanged += new System.EventHandler(this.dropDownTwo_SelectedIndexChanged);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(215, 231);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(96, 41);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert!";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "The Ultimate Convertion Program";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(197, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "by archergs";
+            // 
+            // userValue
+            // 
+            this.userValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userValue.Location = new System.Drawing.Point(215, 176);
+            this.userValue.Name = "userValue";
+            this.userValue.Size = new System.Drawing.Size(100, 24);
+            this.userValue.TabIndex = 5;
+            this.userValue.TextChanged += new System.EventHandler(this.userValue_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(173, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(174, 38);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Initial Measurement";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(351, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "New Measurement";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(245, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Value";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(407, 335);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(122, 23);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "Check For Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 370);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.userValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.dropDownTwo);
+            this.Controls.Add(this.dropDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "The Ultimate Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox dropDownTwo;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userValue;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.ComboBox dropDown;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button updateButton;
+    }
+}
+
---
+++ Conversion App/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Squirrel;
+
+namespace Conversion_App
+{
+    public partial class Form1 : Form
+    {
+        public string selected1;
+        public string selected2;
+
+        public string userInput;
+        public double userNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected1 = dropDown.SelectedItem.ToString();
+        }
+
+        private void dropDownTwo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected2 = dropDownTwo.SelectedItem.ToString();
+        }
+
+        private void userValue_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            userInput = userValue.Text;
+
+            if (selected1 == "mm")
+            {
+                if (selected2 == "mm")
+                {
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "cm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 10;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "m")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 1000;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "km")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 1000000;
+                    userInput = userNum.ToString("0.######");
+                    textBox2.Text = userInput;
+                }
+            }
+
+            if (selected1 == "cm")
+            {
+                if (selected2 == "mm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 10;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "cm")
+                {
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "m")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 100;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "km")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 100000;
+                    userInput = userNum.ToString("0.######");
+                    textBox2.Text = userInput;
+                }
+            }
+
+            if (selected1 == "m")
+            {
+                if (selected2 == "mm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 1000;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "cm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 100;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "m")
+                {
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "km")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum / 1000;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+            }
+
+            if (selected1 == "km")
+            {
+                if (selected2 == "mm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 1000000;
+                    userInput = userNum.ToString("0.#######");
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "cm")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 100000;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "m")
+                {
+                    double.TryParse(userInput, out userNum);
+                    userNum = userNum * 1000;
+                    userInput = userNum.ToString();
+                    textBox2.Text = userInput;
+                }
+
+                if (selected2 == "km")
+                {
+                    textBox2.Text = userInput;
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            CheckForUpdate();
+            async void CheckForUpdate()
+            {
+                try
+                {
+                    using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/dartvalince/DiscerningEye"))
+                    {
+                        UpdateManager updateManager = mgr;
+                        var release = await mgr.UpdateApp();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string message = ex.Message + Environment.NewLine;
+                    if (ex.InnerException != null)
+                        message += ex.InnerException.Message;
+                    MessageBox.Show(message);
+                }
+            }
+        }
+    }
+}
